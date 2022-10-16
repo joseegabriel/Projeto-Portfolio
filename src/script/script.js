3242b215902c1f050b87c6e680d56d7f269e41e7
Jose Gabriel <joseg081@gmail.com>
2022-10-16 16:28:40 -0300

    third commit

---
+++ src/script/script.js
@@ -0,0 +1,13 @@
+const skills = document.querySelectorAll(".mouse-icon")
+
+const content = document.querySelector('.conteudo');
+
+const description = [
+    '<h2>HTML</h2> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p>',
+    '<h2>CSS</h2> <p>É um mecanismo para adicionar estilo a um documento web.</p>',
+]
+
+skills.forEach((elemento, index) => {
+    elemento.addEventListener('mouseover', () => content.innerHTML = `${description[index]}`)
+    elemento.addEventListener('mouseout', () => content.innerHTML = 'Coloque o mouse por cima para ler a descrição')
+});
