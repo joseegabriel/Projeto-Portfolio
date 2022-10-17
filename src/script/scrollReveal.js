a67df218dbcf97ab1e5b2c922ea9cfbf0eaf6f87
Jose Gabriel <joseg081@gmail.com>
2022-10-16 23:04:06 -0300

    Animated divs

---
+++ src/script/scrollReveal.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset: true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.aprensetation-1', {duration : 5000, origin: 'top'})
+sr.reveal('.principal__sentence', {duration : 8000, origin: 'top'})
+sr.reveal('.teste', {duration : 10000, origin: 'top'})
+sr.reveal('.linkedin', {duration : 3000, origin: 'left'})
+sr.reveal('.github', {duration : 3000, origin: 'right'})
+sr.reveal('.projects-wrapper', {duration : 3000, origin: 'bottom'})
+sr.reveal('.info-list', {duration : 3000, origin: 'left'})
+sr.reveal('.habilities', {duration : 3000, origin: 'bottom'})
